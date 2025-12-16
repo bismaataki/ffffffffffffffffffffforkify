a774461723bfa6a307666d934239584b9bbf1a0a
Bisma Iftikhar <bisma@example.com>
2025-12-16 14:48:31 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '941b1f07-431f-406d-a08e-ce098601c735';
+export const MODAL_CLOSE_SEC = 2.5;
+
